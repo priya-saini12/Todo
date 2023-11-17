b670d203701db5147b6ec209ccb4a4be785724f7
priya-saini12 <priyasainiwins@gmail.com>
2023-11-17 09:42:42 +0530

    final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,29 @@
 document.querySelector('#push').onclick = function(){
     if(document.querySelector('#additem').value.length == 0){
-        alert("Enter Task Name!!!!")
+        document.getElementById("error").innerHTML="*Enter Task!!";
     }
-
     else{
-    
-        document.querySelector('.items').innerHTML += `
+        let a = document.querySelector('.items');
+        a.insertAdjacentHTML('afterend',`
            
-                <div class="item d-flex justify-content-between bg-light p-2 rounded-2 mb-3">
+                <div class="item d-flex justify-content-between bg-light rounded-2 mb-3">
            
-             <p class="m-2">
+             <p class="mt-2">
                 ${document.querySelector('#additem').value}
                 </p>
-          <span>
+          <span class="mt-1">
 
-                <button type="button" class="btn btn-success" id="done"><i class="fa-regular fa-circle-check"></i></button>
-            <button type="button" class="btn btn-danger" id="remove"><i class="fa-regular fa-circle-xmark text-white"></i></button>
+                <button type="button" class="btn btn2" id="done"><i class="fa-regular fa-circle-check fa-xs text-white"></i></button>
+            <button type="button" class="btn btn3" id="remove"><i class="fa-regular fa-circle-xmark fa-xs text-white"></i></button>
    </span>
         </div>
-        `;
-     
+        `);
         let remove = document.querySelectorAll("#remove");
         for(let i=0; i<remove.length; i++){
             remove[i].onclick = function(){
                 this.parentNode.parentNode.remove();
             }
         }
-     
         let done = document.querySelectorAll("#done");
         for(let i=0; i<done.length; i++){
             done[i].onclick = function(){
@@ -38,8 +35,7 @@ document.querySelector('#push').onclick = function(){
                 }
             }
             }
+            document.getElementById("error").innerHTML="";
         }
-        
-    
     document.querySelector('#additem').value="";
 }
